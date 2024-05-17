df686876c7a4cd7e0823669d05476c07b3e28d79
Rajvardhan Das <98523794+rajvardhan-das@users.noreply.github.com>
2024-05-17 15:22:39 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 import Layout from "./components/Layout/Layout";
+import axios from 'axios'
 
 function App() {
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    axios.post('voyage-tours-mern-api.vercel.app')
+    .then(result=>console.log(result))
+    .catch(err =>console.log(err))
+  }
   return <Layout />;
 }
 
